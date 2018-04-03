8a2bd8b453109fde0d33f0bc80f2c3df516aded3
samiul <samiulcsedu22@gmail.com>
2018-04-03 15:53:26 +0600

    Added Yandex Translator API

---
+++ Yandex/app/src/main/java/com/example/samiul_siddiqui/mainpackage/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.samiul_siddiqui.mainpackage;
+
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.example.samiul_siddiqui.yandex.R;
+import com.example.samiul_siddiqui.yandex.yandexpackage.Detect;
+import com.example.samiul_siddiqui.yandex.yandexpackage.Language;
+import com.example.samiul_siddiqui.yandex.yandexpackage.Translate;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        textView = (TextView) findViewById(R.id.textview);
+        textView.setText("samiul");
+
+        Translate.setKey("trnsl.1.1.20180324T123828Z.2b9ffa716760ad4e.d9995e9e7e47f773cdc06f3c7ad25139455efe1d");
+
+
+        String translatedText = "";
+        String mytext = "Hola, amigo!";
+
+        try {
+
+            translatedText = Translate.execute(mytext, Detect.execute(mytext), Language.fromString("bn"));
+        } catch (Exception e) {
+        }
+
+
+        try {
+            textView.setText(translatedText + " from " + Detect.execute(mytext));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Yandex/app/src/main/java/com/example/samiul_siddiqui/yandex/yandexpackage/ApiKeys.java
@@ -0,0 +1,5 @@
+package com.example.samiul_siddiqui.yandex.yandexpackage;
+
+public class ApiKeys {
+  public static final String YANDEX_API_KEY = "[Put your API key here]";
+}
